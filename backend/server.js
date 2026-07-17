189e92a901d940cda598590a3853f6c79193bd95
taivippro123 <phanvothanhtai1007@gmail.com>
2026-07-18 04:56:25 +0700

    fix backend ngrok 4

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
-  res.setHeader('ngrok-skip-browser-warning', 'true');
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
+import axios from 'axios'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
