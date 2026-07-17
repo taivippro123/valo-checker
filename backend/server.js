cc86f51300d12fc6e7af8d97fad3612e1692631d
taivippro123 <phanvothanhtai1007@gmail.com>
2026-07-18 04:52:02 +0700

    fix backend ngrok 3

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,10 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader('ngrok-skip-browser-warning', 'true');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 app.use(express.json());
